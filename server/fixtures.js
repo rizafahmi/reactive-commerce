31d570d696e55d08f62d6834ed8a4c41dfc606c4
Riza Fahmi <rizafahmi@gmail.com>
2014-05-02 17:01:10 +0700

    Add data to the products collection.

---
+++ server/fixtures.js
@@ -0,0 +1,28 @@
+Products.insert({
+  name: "Tribal Patterned Skirt",
+  price: 500000,
+  dateCreated: new Date(),
+  publishStatus: 'published',
+  thumb: '1.jpg'
+});
+Products.insert({
+  name: "Abstract Patterned Whitecollar Dress",
+  price: 154000,
+  dateCreated: new Date(),
+  publishStatus: 'published',
+  thumb: '2.jpg'
+});
+Products.insert({
+  name: "Dzine Clothing Zombie Pac Man",
+  price: 454000,
+  dateCreated: new Date(),
+  publishStatus: 'published',
+  thumb: '3.jpg'
+});
+Products.insert({
+  name: "Dzine Clothing I Swallow",
+  price: 954000,
+  dateCreated: new Date(),
+  publishStatus: 'published',
+  thumb: '4.jpg'
+});
