b74446a9cfc11ab5a883642b17d3149f7c4251bf
Riza Fahmi <rizafahmi@gmail.com>
2014-05-02 17:11:39 +0700

    Revise fixture data

--- client/views/products/products_list.js
+++ client/views/products/products_list.js
@@ -1,20 +1,3 @@
-var productsData = [
-{
-  name: "Tribal Patterned Skirt",
-  price: 500000,
-  dateCreated: new Date(),
-  publishStatus: 'published',
-  thumb: '1.jpg'
-},
-{
-  name: "Abstract Patterned Whitecollar Dress",
-  price: 154000,
-  dateCreated: new Date(),
-  publishStatus: 'published',
-  thumb: '2.jpg'
-}
-];
-
 Template.productsList.helpers({
   products: productsData
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -1,28 +1,30 @@
-Products.insert({
-  name: "Tribal Patterned Skirt",
-  price: 500000,
-  dateCreated: new Date(),
-  publishStatus: 'published',
-  thumb: '1.jpg'
-});
-Products.insert({
-  name: "Abstract Patterned Whitecollar Dress",
-  price: 154000,
-  dateCreated: new Date(),
-  publishStatus: 'published',
-  thumb: '2.jpg'
-});
-Products.insert({
-  name: "Dzine Clothing Zombie Pac Man",
-  price: 454000,
-  dateCreated: new Date(),
-  publishStatus: 'published',
-  thumb: '3.jpg'
-});
-Products.insert({
-  name: "Dzine Clothing I Swallow",
-  price: 954000,
-  dateCreated: new Date(),
-  publishStatus: 'published',
-  thumb: '4.jpg'
-});
+if(Products.find().count() === 0){
+  Products.insert({
+    name: "Tribal Patterned Skirt",
+    price: 500000,
+    dateCreated: new Date(),
+    publishStatus: 'published',
+    thumb: '1.jpg'
+  });
+  Products.insert({
+    name: "Abstract Patterned Whitecollar Dress",
+    price: 154000,
+    dateCreated: new Date(),
+    publishStatus: 'published',
+    thumb: '2.jpg'
+  });
+  Products.insert({
+    name: "Dzine Clothing Zombie Pac Man",
+    price: 454000,
+    dateCreated: new Date(),
+    publishStatus: 'published',
+    thumb: '3.jpg'
+  });
+  Products.insert({
+    name: "Dzine Clothing I Swallow",
+    price: 954000,
+    dateCreated: new Date(),
+    publishStatus: 'published',
+    thumb: '4.jpg'
+  });
+}
